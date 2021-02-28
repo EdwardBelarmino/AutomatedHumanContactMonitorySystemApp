5c9b438071360777e8b94a5aa8ad8e7612d55491
Ravni Arador <ravarador@gmail.com>
2021-02-28 21:08:33 +0800

    Added toolstripmenu in MainForm

--- AutomatedHumanContactMonitorySystemApp/Forms/MainForm.Designer.cs
+++ AutomatedHumanContactMonitorySystemApp/Forms/MainForm.Designer.cs
@@ -33,11 +33,17 @@ namespace AutomatedHumanContactMonitorySystemApp
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemAttendance = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemAttendanceAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemAttendee = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemPlace = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnPerson
             // 
-            this.btnPerson.Location = new System.Drawing.Point(0, 2);
+            this.btnPerson.Location = new System.Drawing.Point(24, 52);
             this.btnPerson.Name = "btnPerson";
             this.btnPerson.Size = new System.Drawing.Size(96, 39);
             this.btnPerson.TabIndex = 0;
@@ -46,7 +52,7 @@ namespace AutomatedHumanContactMonitorySystemApp
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(0, 47);
+            this.btnAdd.Location = new System.Drawing.Point(24, 97);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(96, 41);
             this.btnAdd.TabIndex = 1;
@@ -56,7 +62,7 @@ namespace AutomatedHumanContactMonitorySystemApp
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(0, 94);
+            this.btnUpdate.Location = new System.Drawing.Point(24, 144);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(96, 41);
             this.btnUpdate.TabIndex = 2;
@@ -66,13 +72,51 @@ namespace AutomatedHumanContactMonitorySystemApp
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(0, 141);
+            this.btnDelete.Location = new System.Drawing.Point(24, 191);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(96, 41);
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemAttendance,
+            this.toolStripMenuItemAttendee,
+            this.toolStripMenuItemPlace});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(775, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItemAttendance
+            // 
+            this.toolStripMenuItemAttendance.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemAttendanceAdd});
+            this.toolStripMenuItemAttendance.Name = "toolStripMenuItemAttendance";
+            this.toolStripMenuItemAttendance.Size = new System.Drawing.Size(80, 20);
+            this.toolStripMenuItemAttendance.Text = "Attendance";
+            // 
+            // toolStripMenuItemAttendanceAdd
+            // 
+            this.toolStripMenuItemAttendanceAdd.Name = "toolStripMenuItemAttendanceAdd";
+            this.toolStripMenuItemAttendanceAdd.Size = new System.Drawing.Size(160, 22);
+            this.toolStripMenuItemAttendanceAdd.Text = "Add Attendance";
+            // 
+            // toolStripMenuItemAttendee
+            // 
+            this.toolStripMenuItemAttendee.Name = "toolStripMenuItemAttendee";
+            this.toolStripMenuItemAttendee.Size = new System.Drawing.Size(67, 20);
+            this.toolStripMenuItemAttendee.Text = "Attendee";
+            // 
+            // toolStripMenuItemPlace
+            // 
+            this.toolStripMenuItemPlace.Name = "toolStripMenuItemPlace";
+            this.toolStripMenuItemPlace.Size = new System.Drawing.Size(47, 20);
+            this.toolStripMenuItemPlace.Text = "Place";
             // 
             // MainForm
             // 
@@ -83,10 +127,15 @@ namespace AutomatedHumanContactMonitorySystemApp
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnPerson);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +145,11 @@ namespace AutomatedHumanContactMonitorySystemApp
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAttendance;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAttendanceAdd;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAttendee;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemPlace;
     }
 }
 
