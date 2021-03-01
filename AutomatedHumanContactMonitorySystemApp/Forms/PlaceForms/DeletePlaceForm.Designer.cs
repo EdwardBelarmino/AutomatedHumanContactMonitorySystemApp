c2a5dd2f1ff17c3cca9acaacfbe6da61cf8ca410
King Edward Belarmino <portalknights94@gmail.com>
2021-03-01 14:09:58 +0800

    Created Place Forms

---
+++ AutomatedHumanContactMonitorySystemApp/Forms/PlaceForms/DeletePlaceForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace AutomatedHumanContactMonitorySystemApp.Forms.PlaceForms
+{
+    partial class DeletePlaceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DeletePlaceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 450);
+            this.Name = "DeletePlaceForm";
+            this.Text = "DeletePlaceForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
