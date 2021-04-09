8020b54c4b056e4c725c15294374f6109bee6d4e
Ravni Arador <ravarador@gmail.com>
2021-04-09 15:39:55 +0800

    Added new UI for mainform (ParentForm)

---
+++ AutomatedHumanContactMonitorySystemApp/Forms/ParentForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace AutomatedHumanContactMonitorySystemApp.Forms
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ParentForm));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnContactTracing = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnToggleMenu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(67)))), ((int)(((byte)(255)))));
+            this.panelMenu.Controls.Add(this.btnContactTracing);
+            this.panelMenu.Controls.Add(this.btnAdmin);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Controls.Add(this.panel2);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(178, 590);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnContactTracing
+            // 
+            this.btnContactTracing.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnContactTracing.FlatAppearance.BorderSize = 0;
+            this.btnContactTracing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContactTracing.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnContactTracing.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnContactTracing.Image = ((System.Drawing.Image)(resources.GetObject("btnContactTracing.Image")));
+            this.btnContactTracing.Location = new System.Drawing.Point(0, 169);
+            this.btnContactTracing.Name = "btnContactTracing";
+            this.btnContactTracing.Size = new System.Drawing.Size(178, 55);
+            this.btnContactTracing.TabIndex = 3;
+            this.btnContactTracing.Text = "Contact Tracing";
+            this.btnContactTracing.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnContactTracing.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnContactTracing.UseVisualStyleBackColor = true;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAdmin.FlatAppearance.BorderSize = 0;
+            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdmin.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAdmin.Image = ((System.Drawing.Image)(resources.GetObject("btnAdmin.Image")));
+            this.btnAdmin.Location = new System.Drawing.Point(0, 114);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(178, 55);
+            this.btnAdmin.TabIndex = 2;
+            this.btnAdmin.Text = "Admin";
+            this.btnAdmin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdmin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 59);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(178, 55);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.btnToggleMenu);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(178, 59);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnToggleMenu
+            // 
+            this.btnToggleMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(67)))), ((int)(((byte)(255)))));
+            this.btnToggleMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnToggleMenu.FlatAppearance.BorderSize = 0;
+            this.btnToggleMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToggleMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnToggleMenu.Image")));
+            this.btnToggleMenu.Location = new System.Drawing.Point(0, 0);
+            this.btnToggleMenu.Name = "btnToggleMenu";
+            this.btnToggleMenu.Size = new System.Drawing.Size(41, 59);
+            this.btnToggleMenu.TabIndex = 1;
+            this.btnToggleMenu.UseVisualStyleBackColor = false;
+            this.btnToggleMenu.Click += new System.EventHandler(this.btnToggleMenu_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(26, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AHCMS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(178, 59);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(614, 531);
+            this.panel3.TabIndex = 1;
+            // 
+            // ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(792, 590);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ParentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ParentForm";
+            this.panelMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnContactTracing;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnToggleMenu;
+    }
+}
--- AutomatedHumanContactMonitorySystemApp/Program.cs
+++ AutomatedHumanContactMonitorySystemApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AutomatedHumanContactMonitorySystemApp.Forms;
 using AutomatedHumanContactMonitorySystemApp.Forms.AttendeeForms;
 using AutomatedHumanContactMonitorySystemApp.IRepositories;
 using AutomatedHumanContactMonitorySystemApp.Repositories;
@@ -22,17 +23,17 @@ namespace AutomatedHumanContactMonitorySystemApp
         static void Main()
         {
             container = new Container();
-            container.Register<IAttendeeRepository, AttendeeRepository>(Lifestyle.Singleton);
-            container.Register<IPlaceRepository, PlaceRepository>(Lifestyle.Singleton);
-            container.Register<IAttendanceRepository, AttendanceRepository>(Lifestyle.Singleton);
-            container.Register<IAppUserRepository, AppUserRepository>(Lifestyle.Singleton);
+            //container.Register<IAttendeeRepository, AttendeeRepository>(Lifestyle.Singleton);
+            //container.Register<IPlaceRepository, PlaceRepository>(Lifestyle.Singleton);
+            //container.Register<IAttendanceRepository, AttendanceRepository>(Lifestyle.Singleton);
+            //container.Register<IAppUserRepository, AppUserRepository>(Lifestyle.Singleton);
 
-            //container.Register<AddAttendeeForm>(Lifestyle.Singleton);
-            container.Register<MainForm>(Lifestyle.Singleton);
+            ////container.Register<AddAttendeeForm>(Lifestyle.Singleton);
+            container.Register<ParentForm>(Lifestyle.Singleton);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(container.GetInstance<MainForm>());
+            Application.Run(container.GetInstance<ParentForm>());
         }
     }
 }
