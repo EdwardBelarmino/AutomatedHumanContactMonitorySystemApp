99407bf5d1984e7435330ec3493034c887742218
Ravni Arador <ravarador@gmail.com>
2021-04-15 09:38:06 +0800

    Fix Status in ContactTracingForm

--- AutomatedHumanContactMonitorySystemApp/Forms/ContactTracingForm.cs
+++ AutomatedHumanContactMonitorySystemApp/Forms/ContactTracingForm.cs
@@ -63,7 +63,7 @@ namespace AutomatedHumanContactMonitorySystemApp.Forms
                 VisitedDateTime = DateTime.Now,
                 Temperature = double.Parse(txtTemperature.Text),
                 AttendeeId = selectedAttendeeId,
-                Status = "NORMAL",
+                Status = selectedAttendeeStatus,
                 PlaceId = 1
             };
 
